269980c7c7978420b4b9cb168502a8e2dac4270b
mike <d.mike.s@yandex.ru>
2025-08-01 00:10:20 +0300

    102 третий  коммит

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MyLibrary
 
-# Register your models here.
+
+
+
+
+
+@admin.register(MyLibrary)
+class MylibraryAdmin(admin.ModelAdmin):
+    list_display = ('avtor','composition', 'year')
+    search_fields = ('avtor','composition', 'year')
+    list_filter = ('avtor','composition', 'year')
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class MyLibrary(models.Model):
+    avtor = models.CharField(max_length=100, verbose_name="Автор")
+    composition = models.CharField(max_length=100, verbose_name="Композиция")
+    year = models.DateField(verbose_name="Дата произведения")
